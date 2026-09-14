2762ba9c1204571c91ef7b6e655670a60e3a3a92
tstewart08 <tstewart50@student.rccd.edu>
2026-09-13 19:58:03 -0700

    school import

---
+++ CSC_CIS_17a-master/Class/ClassMarkSort_V3_Dynamic_Array_of_Objects/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=classmarksort_v3_dynamic_array_of_objects
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/classmarksort_v3_dynamic_array_of_objects
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=classmarksortv3dynamicarrayofobjects.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/classmarksortv3dynamicarrayofobjects.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=classmarksort_v3_dynamic_array_of_objects
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/classmarksort_v3_dynamic_array_of_objects
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=classmarksortv3dynamicarrayofobjects.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/classmarksortv3dynamicarrayofobjects.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ CSC_CIS_17a-master/Project/README.md
@@ -0,0 +1,2 @@
+# CSC_CIS_17A
+C++ Programming
